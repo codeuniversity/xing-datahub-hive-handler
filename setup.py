66a3491066b1ee90041bdb923732c60bbf3858f4
alexmorten <alexandrius.martinus@gmx.de>
2018-03-14 17:54:33 +0100

    bring python package stuff back

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hive_handler',
+      version='0.1',
+      description='Handler for the hive tables',
+      url='https://github.com/codeuniversity/xing-datahub-hive-handler',
+      author='Alexander MArtin',
+      license='MIT',
+      packages=['xing_datahub_hive_handler', 'model'],
+      zip_safe=False)
