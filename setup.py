ddddfe81103a3f77acbfa633880d80e2ed2eabf0
alexmorten <alexandrius.martinus@gmx.de>
2018-03-14 18:02:05 +0100

    rename

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='hive_handler',
       version='0.1',
       description='Handler for the hive tables',
-      url='https://github.com/codeuniversity/xing-datahub-hive-handler',
+      url='https://github.com/codeuniversity/xing_datahub_hive_handler',
       author='Alexander MArtin',
       license='MIT',
-      packages=['xing_datahub_hive_handler', 'model'],
+      packages=['hive_handler', 'model'],
       zip_safe=False)
